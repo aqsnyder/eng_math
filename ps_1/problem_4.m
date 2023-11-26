77ef3b9b04900a7f884383baeba0612ca9e9a9bd
Snyder <aaron.q.snyder@gmail.com>
2023-11-26 18:45:04 -0500

    starting second compilation

--- ps_1/problem_4.m
+++ ps_1/problem_4.m
@@ -1,59 +1,45 @@
-function compare_solutions_without_fsolve()
-    % Define the u1 and u2 using Bessel functions
-    u1 = @(x) 1/sqrt(x) .* besseli(0, 2 * sqrt(x));
-    u2 = @(x) 1/sqrt(x) .* besselk(0, 2 * sqrt(x));
+function y=f_ex1_check()
+% ======= bessel trick solution ============
+xx = linspace(1,5,201);
+u1 = @(x) besseli(0,2*sqrt(x)); % potential error
+u2 = @(x) besselk(0,2*sqrt(x)); % potential error
+CC = [u1(1) u2(1); u1(5) u2(5)]\[-10;-18];
+besseltrick=@(x) CC(1)*u1(x) + CC(2)*u2(x);
 
-    % Compute u1 and u2 at given boundary points (1 and 5)
-    u1_values = [u1(1), u1(5)];
-    u2_values = [u2(1), u2(5)];
+plot(xx, besseltrick(xx), 'DisplayName', 'Bessel Trick'); hold on
+% ======= series solution ============
+xx = linspace(1,5,51);
+u1 = @(x) f_ex1(x,1);
+u2 = @(x) f_ex1(x,2);
+CC = [u1(1) u2(1); u1(5) u2(5)]\[-10;-18];
+seriessol=@(x) CC(1)*u1(x) + CC(2)*u2(x);
+plot(xx, seriessol(xx), '+', 'DisplayName', 'Series Solutuion'); hold off
+%--------------------------------------------------------------------------
 
-    % Solve the system of equations to determine coefficients using boundary conditions
-    CC = linsolve([u1_values; u2_values]', [-10; -18]);
-
-    % Define the solution using Bessel
-    bessel_trick = @(x) CC(1) * u1(x) + CC(2) * u2(x);
-
-    % Define the series solution to the differential equation using the Frobenius method
-    function y_val = series(x_val, N)
-        if nargin < 2
-            N = 50;
-        end
-        
-        lambda_1 = 0;
-        lambda_2 = -1;
-        
-        C1 = zeros(1, N);
-        C2 = zeros(1, N);
-        
-        C1(1) = 1;
-        C2(2) = 1;
-        
-        for k = 3:N
-            C1(k) = -C1(k-1) / (k*(k-1)*(2*k));
-            C2(k) = -C2(k-1) / ((k-1)*(k-2)*(2*k-2));
-        end
-
-        y = @(x, A, B) sum(A .* C1 .* (x.^(0:N-1)) + B .* C2 .* (x.^(1:N)));
-        
-        coef_matrix = [y(1, 1, 0), y(1, 0, 1); y(5, 1, 0), y(5, 0, 1)];
-        rhs = [-10; -18];
-        AB = coef_matrix \ rhs;
-
-        y_val = y(x_val, AB(1), AB(2));
-    end
-
-    x = linspace(1, 5, 400);
-    y_vals_series = arrayfun(@(val) series(val), x);
-    y_vals_bessel = arrayfun(bessel_trick, x);
-
-    % Plotting the results
-    figure;
-    plot(x, y_vals_series, 'DisplayName', 'Series Solution');
-    hold on;
-    plot(x, y_vals_bessel, 'DisplayName', 'Bessel Solution');
-    xlabel('x');
-    ylabel('y');
-    legend;
-    title('Comparison of Series and Bessel Solutions Without fsolve');
-    grid on;
+function y=f_ex1(x,which)
+% ======= (1) ==================
+%ff =@(k) -1/((k-2)*(k-1)+(2*(k-1))); % most likely potential error
+ff =@(k) -1/(k*(k+1))
+xmax = max(abs(x));
+NN=2;
+while abs(xmax*ff(NN))>1e-3, NN=NN+1; end
+y1 = ones(size(x));
+for kk=NN:-1:1
+    y1 = 1 + ff(kk)*x.*y1;
 end
+if which==1, y=y1; return; end
+% === second solution ==========
+c0 = 1; cc=zeros(1,NN); cc(1) = ff(1)*c0';
+d0 = 1; dd=zeros(1,NN); dd(1) = (d0 - 2*cc(1)*1)/(1)^2; % potential error
+for kk=2:NN
+    cc(kk) = ff(kk)*cc(kk-1);
+    dd(kk) = (-dd(kk-1) - 2*cc(kk)*kk) / (kk^2+kk); % potential error
+end
+dd = [dd(end:-1:1) d0];
+y = log(x).*y1 + polyval(dd,x);
+
+% Add title and axis labels
+title('Comparison of Bessel Trick and Series Solutions');
+xlabel('x');
+ylabel('y');
+legend();
