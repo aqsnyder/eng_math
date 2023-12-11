a40ed288812fcfdaebd130ead92bfb440cabc382
Snyder <aaron.q.snyder@gmail.com>
2023-12-10 22:12:41 -0500

    problem 1 and 2 complete

--- ps_2/problem_1_soln.m
+++ ps_2/problem_1_soln.m
@@ -1,10 +1,16 @@
-%===================================
 % List of Nterms to run
 NtermsList = [5, 20, 40];
 
-% Execute problem_1 for each Nterms
-for N = NtermsList
-    figure;
+% Number of subplots
+numSubplots = length(NtermsList);
+
+% Create a figure for the subplots
+figure;
+
+% Execute problem_1 for each Nterms and place each in a subplot
+for i = 1:numSubplots
+    N = NtermsList(i);
+    subplot(1, numSubplots, i); % Adjust the subplot grid as needed
     problem_1(N);
-    title(['Q(x) eigenfunction series expansion with ', num2str(N), ' Terms'])
+    title(['Q(x) eigenfunction series expansion with ', num2str(N), ' Terms']);
 end
--- ps_2/problem_2.m
+++ ps_2/problem_2.m
@@ -1,57 +1,50 @@
-function problem_2(Nterms)
-    % Main function
+function problem_2(terms_to_use)
+    w = @(x) x.^3;
+    figure; % Open a new figure window
+    for index = 1:length(terms_to_use)
+        lam = getlam(terms_to_use(index));
+        Nx = 4*length(lam); if Nx<100, Nx=100; end
+        dx = 4/Nx; xx = 0:dx:4;
+        gg = QQ(xx); % Updated to use the new function QQ
+        frep = zeros(size(xx));
+        yn = zeros(size(xx));
 
-    nint = 3 * max(Nterms);
-    dx = 4 / nint; 
-    xx = 0:dx:4;
-    lam = get_roots(max(Nterms));
-
-    for icase = 1:length(Nterms)
-        NN = Nterms(icase);
-        disp(NN);
-        qq = zeros(size(xx));
-        for nn = 1:NN
-            YY = besselj(0, lam(nn) * xx);
-            ww = xx;
-            Nhat = simpson(ww .* YY .* YY, dx);
-            Qhat = simpson(ww .* YY .* QQ(xx), dx);
-            qq = qq + Qhat / Nhat * YY;
+        for ll = lam
+            yn(1) = ll/2;
+            yn(2:end) = besselj(1, ll*xx(2:end))./xx(2:end);
+            NN = simp_int(yn.^2.*w(xx),dx);
+            fhat = simp_int(yn.*gg.*w(xx),dx);
+            frep = frep + fhat*yn/NN;
         end
-        subplot(2, 2, icase);
-        plot(xx, qq); 
-        hold on;
-        plot(xx, QQ(xx), 'k-', 'linewidth', 2); 
-        hold off;
-        title(sprintf('# terms = %d', NN));
-    end
 
-    function foo = QQ(x)
-        % Nested function for QQ
-        foo = zeros(size(x));
-        foo(x >= 1 & x < 2) = abs(sin(6 * (x(x >= 1 & x < 2) - 1)));
-        foo(x >= 2 & x < 3) = sqrt(x(x >= 2 & x < 3) - 2) - sin(6);
-        foo(x >= 3) = 0;
-    end
-
-    function l = simpson(f, dx)
-        % Nested function for Simpson's rule
-        NN = length(f);
-        l = dx / 3 * (f(1) + f(NN) + 4 * sum(f(2:2:NN - 1)) + 2 * sum(f(3:2:NN - 2)));
-    end
-
-    function lams = get_roots(NN)
-        % Nested function for finding roots
-        ff = @(x) besselj(0, 4 * x);
-        dx = 1 / 10; x = 0;
-        lams = zeros(1, NN);
-        iroot = 0;
-        while iroot < NN
-            while ff(x) * ff(x + dx) > 0, x = x + dx; end
-            iroot = iroot + 1;
-            lams(iroot) = fzero(ff, x);
-            x = lams(iroot) + dx;
-        end
+        subplot(2, 2, index); % Create a subplot for each term
+        plot(xx, frep, ':', xx, gg, '-k', 'linewidth', 2);
+        title(sprintf('Using %d terms of eigens', terms_to_use(index)));
     end
 end
 
-%problem_2([5, 10, 20, 40])
+function foo = simp_int(a, dx)
+    foo = dx/3*(a(1)+a(end)+4*sum(a(2:2:end-1))+2*sum(a(3:2:end-2)));
+end
+
+function lams = getlam(N)
+    lams = zeros(1, N); iroot = 0;
+    ff = @(l) 0.5*(besselj(0, 4*l) - besselj(2, 4*l));
+    x = 0; dx = 1/10;
+    while iroot < N
+        while ff(x) * ff(x+dx) > 0, x = x + dx; end
+        iroot = iroot + 1;
+        lams(iroot) = fzero(ff, x);
+        x = x + 2*dx;
+    end
+end
+
+function foo = QQ(x)
+    % Nested function for QQ
+    foo = zeros(size(x));
+    foo(x >= 1 & x < 2) = abs(sin(6 * (x(x >= 1 & x < 2) - 1)));
+    foo(x >= 2 & x < 3) = sqrt(x(x >= 2 & x < 3) - 2) - sin(6);
+    foo(x >= 3) = 0;
+end
+
+%problem_2([5, 10, 20, 40]);
